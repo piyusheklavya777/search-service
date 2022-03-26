641ff69ff54061a29638f8ec73573e9376c3ee82
Piyush Eklavya <piyusheklavya1@gmail.com>
2022-03-27 02:40:54 +0530

    text extract aws sdk wrapper function

--- packages/utilities/aws-sdk-utilities.js
+++ packages/utilities/aws-sdk-utilities.js
@@ -4,6 +4,13 @@ const logger = require('./logger');
 
 // AWS.config.update({ region: 'ap-southeast-1' });
 
+const {
+  ACCOUNT_ID,
+  TEXT_EXTRACTED_NOTIFICATION_SNS,
+  REGION,
+  PUBLISH_TO_SNS_ROLE,
+} = process.env;
+
 async function listFilesFromS3Bucket({ bucketName }) {
   const s3 = new AWS.S3();
   let response;
@@ -14,7 +21,10 @@ async function listFilesFromS3Bucket({ bucketName }) {
       })
       .promise();
   } catch (e) {
-    logger.error('Error occurred while trying to fetch object list from s3: ', e);
+    logger.error(
+      'Error occurred while trying to fetch object list from s3: ',
+      e,
+    );
     return [];
   }
   return response;
@@ -24,17 +34,61 @@ async function loadFileFromS3({ bucketName, fileName }) {
   const s3 = new AWS.S3();
   let response;
   try {
-    response = await s3.getObject({
-      Bucket: bucketName,
-      Key: fileName,
-    }).promise();
+    response = await s3
+      .getObject({
+        Bucket: bucketName,
+        Key: fileName,
+      })
+      .promise();
   } catch (e) {
     logger.error('Error occurred while trying to load files from s3: ', e);
   }
   return _.get(response, 'Body');
 }
 
+async function startExtractText({ bucketName, fileName }) {
+  if (!fileName || !bucketName)
+    throw new Error('call to extractText function missing necessary params: ', {
+      bucketName,
+      fileName,
+    });
+
+  logger.info(
+    `calling aws textract sdk to find text from the document ${fileName}`,
+  );
+
+  const textract = new AWS.Textract();
+
+  const params = {
+    DocumentLocation: {
+      S3Object: {
+        Bucket: bucketName,
+        Name: fileName,
+      },
+    },
+    FeatureTypes: ['TABLES', 'FORMS'],
+    NotificationChannel: {
+      RoleArn: `arn:aws:iam::${ACCOUNT_ID}:role/${PUBLISH_TO_SNS_ROLE}`,
+      SNSTopicArn: `arn:aws:sns:${REGION}:${ACCOUNT_ID}:${TEXT_EXTRACTED_NOTIFICATION_SNS}`,
+    },
+  };
+
+  logger.info('parameters for aws textract startDocumentAnalysis: ', params);
+
+  try {
+    const syncResponse = await textract.startDocumentAnalysis(params).promise();
+    logger.info('response from AWS Textract', syncResponse);
+  } catch (error) {
+    logger.silly(
+      'error while calling AWS textract startDocumentAnalysis',
+      error,
+    );
+    throw error;
+  }
+}
+
 module.exports = {
   listFilesFromS3Bucket,
   loadFileFromS3,
+  startExtractText,
 };
